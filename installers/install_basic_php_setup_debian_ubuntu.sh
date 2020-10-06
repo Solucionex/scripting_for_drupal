2e65c4b00c1f9df735239d2322624a775163ad7e
davidjguru <davidjguru@gmail.com>
2020-10-06 16:36:51 +0200

    (BSH) - Adds a new little script for installing basic PHP 7.4 setup.

---
+++ installers/install_basic_php_setup_debian_ubuntu.sh
@@ -0,0 +1,32 @@
+  sudo apt --qq update &&
+  sudo apt -qq upgrade && 
+  sudo apt -yqq install   \  
+  php7.4 		          \
+  php7.4-bcmath           \
+  php7.4-bz2              \
+  php7.4-curl 		      \
+  php7.4-dev 		      \
+  php7.4-gd 		      \
+  php7.4-dom		      \
+  php7.4-imap             \
+  php7.4-imagick          \
+  php7.4-intl 		      \
+  php7.4-json 		      \
+  php7.4-ldap 		      \
+  php7.4-mbstring	      \
+  php7.4-mysql		      \
+  php7.4-oauth		      \
+  php7.4-odbc		      \
+  php7.4-uploadprogress   \
+  php7.4-ssh2		      \
+  php7.4-xml		      \
+  php7.4-yaml		      \
+  php7.4-zip		      \
+  php7.4-solr		      \
+  php7.4-apcu		      \
+  php7.4-opcache	      \
+  php7.4-redis		      \
+  php7.4-memcache 	      \
+  php7.4-memcached 	      \
+  php7.4-xdebug	          \
+  php7.4-sqlite3          
